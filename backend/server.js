45bfce9e9438259f07bf84cc0f480eb873f38a49
FacuM87 <facundo.mingorance@hotmail.com>
2025-06-22 22:26:39 -0300

    adding EJS

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,13 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-app.use('/', indexRouter)
+app.use('/api', indexRouter)
 
+app.set("view engine", "ejs")
+
+app.use('/', (req,res)=>{
+    res.render('index')
+})
 app.listen(config.port, ()=>{
     console.log(`Server running in ${config.port}`);
 })
