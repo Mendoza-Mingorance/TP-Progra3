e8fffbcf578c0958d0633b294f2b2c37a1b7f2a1
Brayan Mendoza <95663591+BrayanMen@users.noreply.github.com>
2025-06-23 18:31:36 -0300

    Update db.js

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,8 +1,11 @@
 import mysql from 'mysql2/promise';
 import config from '../config/config.js';
-import path from 'path'
-import fs from 'fs'
-import { error } from 'console';// esta importacion fue por error?
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const { db } = config;
 
@@ -12,36 +15,57 @@ const connection = mysql.createPool({
     user: db.user,
     password: db.password,
     waitForConnections: true,
-    // connectionLimit: 10, -> esto creo q no hace falta aclararlo, tengo entendido q es el comportamiento por defecto de mysql2 (a menos que quisieramos especificarle una cantidad distinta, pero en nuestro caso no tiene sentido). Esto habria que chequear si es asi, creo que si, pero por las dudas nomas.
-    // queueLimit: 0,
 });
 
-const conectionInitialDatabase = async ()=>{
-    let connect; // cual es la utilidad de esta variable por fuera del try? se podria ponerle un const al connect dentro del try y eliminar este let?
-    try{
-        connect = await mysql.createConnection({ // con createPool ya tenemos creadas unas 10 conexiones, en principio no hace falta crear otra, podemos agarrar una de las que ya creamos usando await db.getConnection() (asi seco, sin necesidad de pasarle data).
+const conectionInitialDatabase = async () => {
+    try {
+        const connectTemp = await mysql.createConnection({
             host: db.host,
             user: db.user,
-            password:db.password,
-        });
+            password: db.password,
+        }); //Creo la conexion temporal en caso de que no este creada la base de datos
 
-        await connect.query(`CREATE DATABASE IF NOT EXISTS ${connection.database}`);
-        await connect.query(`USE ${connection.database}`);
-        // para las dos lineas de arriba: connection.database no existe. mysql.createPool(...) no crea un objeto 'connection' con un atributo 'database'. createPool es un metdodo al que se le pasa un objeto con la data de la conexion y si todo sale bien, devuelve un conjunto de posibles conexiones a la base de datos. si la idea es acceder al nombre de la db, por como lo tenemos armado creo que iria db.name
+        await connectTemp.query(`CREATE DATABASE IF NOT EXISTS ${db.name}`);
+        await connectTemp.end();
 
-        const scriptSQL = fs.readFileSync(path.join(__dirname, 'schema.sql'), 'utf8')
-        await connect.query(scriptSQL)
+        const connect = await connection.getConnection(); //Tomo la conexion ya hecha en el CreatePool para hacer los inserts
 
-        //Posiblemente condicionarlo a que solo insert los haga en Desarrollo
-        const insertsSQL = fs.readFileSync(path.join(__dirname, 'inserts.sql'), 'utf8')
-        await connect.query(insertsSQL)
+        const scriptSQL = fs.readFileSync(path.join(__dirname, 'schema.sql'), 'utf8');
+
+        const createsDbScript = scriptSQL
+            .split(';')//Corto el array cada sentencia que termina
+            .map(s => s.trim())// limpio los espacios en blanco
+            .filter(Boolean);//solo me traigo las lineas que esten llenas 
+
+        for (const ele of createsDbScript) {
+            await connect.query(ele);
+        }
+
+        if (config.env === 'development') {
+            //Solo hago los inserts en modo desarrollo para que no afecte en caso de que lo pasemos a produccion
+            const [categories] = await connect.query("SELECT COUNT(*) AS total FROM categories")
+            //Reviso si la tabla de categories esta llena
+            // console.log(categories);           
+            if(categories[0].total === 0){//Si no esta llena, cargo el script SQL 
+                const insertsSQL = fs.readFileSync(path.join(__dirname, 'inserts.sql'), 'utf8');
+                const inserts = insertsSQL
+                    .split(';') //Corto el array en cada sentencia que termina ;
+                    .map(s => s.trim()) // limpio los espacios en blanco
+                    .filter(Boolean);//solo me traigo las lineas que esten llenas 
+    
+                for (const ele of inserts) {
+                    await connect.query(ele);
+                }        
+            }
+            // console.log('Datos de insertados.');
+        }
         console.log('database initialized successfully...');
-        
-        
-    }catch(err){
-        // throw new Error pareciera estar armado con la estructura de un console.error. Para pasar el mensaje de error instanciando la clase Error la forma seria algo tipo: throw new Error(`Error de conexion de DB: %{err.message}`). 
-        throw new Error("Error de conexion de DB: ", err)       
+    } catch (err) {
+        console.error('Error de conexion de DB: ', err);
     }
-}
+};
 
-export  {connection, conectionInitialDatabase};
+conectionInitialDatabase();
+
+export { connection, conectionInitialDatabase };
+
