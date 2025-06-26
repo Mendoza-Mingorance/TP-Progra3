86583a125621d008431223a75b286d2e58079258
BrayanMen <brayanjmr880@gmail.com>
2025-06-26 09:01:29 -0300

    Add more routes in products.router

--- backend/routes/products.router.js
+++ backend/routes/products.router.js
@@ -1,15 +1,23 @@
-import {Router} from 'express'
-import { createProduct, deleteProduct, getProductById, getProducts, updateProduct } from '../controllers/products.controller.js'
+import { Router } from 'express';
+import {
+    changeProductsAvailable,
+    createProduct,
+    deleteProduct,
+    getProductById,
+    getProducts,
+    getProductsActive,
+    getProductsInactive,
+    updateProduct,
+} from '../controllers/products.controller.js';
 
-const router = Router()
+const router = Router();
 
-router.route('/')
-    .get(getProducts)
-    .post(createProduct)
+router.route('/').get(getProducts).post(createProduct);
 
-router.route('/:id')
-    .get(getProductById)
-    .patch(updateProduct)
-    .delete(deleteProduct)
+router.get('/active', getProductsActive);
+router.get('/inactive', getProductsInactive);
+router.patch('/status/:id', changeProductsAvailable);
 
-export default router
+router.route('/:id').get(getProductById).patch(updateProduct).delete(deleteProduct);
+
+export default router;
