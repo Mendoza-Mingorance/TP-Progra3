52a1a473b1eba9754ad2d803fd4caee1bb2f7bf3
BrayanMen <brayanjmr880@gmail.com>
2025-07-04 19:28:51 -0300

    Add Config Multer

---
+++ backend/src/api/middlewares/uploadImg.js
@@ -0,0 +1,30 @@
+import multer from 'multer';
+import path from 'path';
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'src/uploads/');
+    },
+    filename: (req, file, cb) => {
+        const nameImgFile = Date.now() + path.extname(file.originalname);
+        cb(null, 'product-' + nameImgFile);
+    },
+});
+
+const fileFormat = (req, file, cb) => {
+    const typesAllows = ['.jpeg', '.jpg', '.png', '.webp'];
+    const ext = path.extname(file.originalname).toLowerCase();
+    const mime = file.mimetype;
+
+    if (typesAllows.includes(ext) && typesAllows.includes(mime)) {
+        cb(null, true);
+    } else {
+        cb(new Error('Solo se aceptan imagenes jpeg, jpg, png y webp'));
+    }
+};
+
+export const uploadFile = multer({
+    storage,
+    fileFormat,
+    limits: { fileSize: 5 * 1024 * 1024 },
+});
