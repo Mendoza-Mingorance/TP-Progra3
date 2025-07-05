1e53fa68a106d314ef3ead01d6a3dadf7d788a1c
FacuM87 <facundo.mingorance@hotmail.com>
2025-07-05 19:44:16 -0300

    incorpora modal para la activacion de productos

--- backend/src/public/js/dashboard.js
+++ backend/src/public/js/dashboard.js
@@ -3,8 +3,16 @@ const deleteModal = document.getElementById('deleteModal');
 const cancelDelete = document.getElementById('cancelDelete');
 const deleteForm = document.getElementById('deleteForm');
 const deleteProductIdInput = document.getElementById('deleteProductId');
+
 const activateButtons = document.querySelectorAll('.activateBtn');
 const confirmDelete = document.getElementById('confirmDelete');
+const activateModal = document.getElementById('activateModal');
+const cancelActivate = document.getElementById('cancelActivate');
+const confirmActivate = document.getElementById('confirmActivate');
+const activateProductIdInput = document.getElementById('activateProductId');
+
+
+// Modal de eliminacion de productos
 
 deleteButtons.forEach((btn) => {
   btn.addEventListener('click', () => {
@@ -38,24 +46,6 @@ deleteForm.addEventListener('submit', async (e) => {
   }
 });
 
-activateButtons.forEach(button => {
-  button.addEventListener('click', async () => {
-    const productId = button.getAttribute('data-id');
-    try {
-      const res = await fetch(`/admin/products/activate/${productId}`, {
-        method: 'POST'
-      });
-      if (res.ok) {
-        window.location.reload(); 
-      } else {
-        console.error('Error en la respuesta de activación');
-      }
-    } catch (err) {
-      console.error('Error al enviar solicitud de activación', err);
-    }
-  });
-});
-
 confirmDelete.addEventListener('click', async () => {
   const productId = deleteProductIdInput.value;
 
@@ -74,3 +64,42 @@ confirmDelete.addEventListener('click', async () => {
     console.error('Error al enviar solicitud de eliminación', err);
   }
 });
+// -------------------------- //
+
+// Modal de activacion de productos
+
+activateButtons.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    const productId = btn.dataset.id;
+    activateProductIdInput.value = productId;
+    activateModal.classList.remove('hidden');
+  });
+});
+
+
+cancelActivate.addEventListener('click', () => {
+  activateModal.classList.add('hidden');
+});
+
+
+confirmActivate.addEventListener('click', async () => {
+  const productId = activateProductIdInput.value;
+
+  try {
+    const res = await fetch(`/admin/products/activate/${productId}`, {
+      method: 'POST'
+    });
+
+    if (res.ok) {
+      activateModal.classList.add('hidden');
+      window.location.reload();
+    } else {
+      console.error('Error al activar el producto');
+    }
+  } catch (err) {
+    console.error('Error en solicitud de activación', err);
+  }
+});
+
+// -------------------------------- //
+
