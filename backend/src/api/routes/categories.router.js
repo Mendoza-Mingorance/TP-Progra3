4252ace434866566c5040cd48b76defb5c47627f
BrayanMen <brayanjmr880@gmail.com>
2025-07-06 13:46:54 -0300

    categories model/controller/routes checked

--- backend/src/api/routes/categories.router.js
+++ backend/src/api/routes/categories.router.js
@@ -1,4 +1,17 @@
 import { Router } from 'express';
+import {
+    createCategory,
+    deleteCategory,
+    getCategories,
+    getCategoriesById,
+    updateCategory,
+} from '../controllers/categories.controller.js';
 const router = Router();
+
+router.get('/', getCategories);
+
+router.post('/', createCategory);
+
+router.route('/:id').get(getCategoriesById).put(updateCategory).delete(deleteCategory);
 
 export default router;
