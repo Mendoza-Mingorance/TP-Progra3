c41cc7c08300963d75ea4a016a6506eee8329960
FacuM87 <facundo.mingorance@hotmail.com>
2025-07-20 23:49:23 -0300

    elimino codigo comentado innecesario

--- backend/server.js
+++ backend/server.js
@@ -6,16 +6,12 @@ import { sqlConnection } from './src/api/database/db.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
 import { fileURLToPath } from 'url';
-// import { checkUploadDir } from './src/api/middlewares/uploadImg.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-// const uploadPath = path.join(__dirname, 'src/public/uploads')
-
 
 const app = express();
 
-// app.use(checkUploadDir(uploadPath));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'src/public')));
